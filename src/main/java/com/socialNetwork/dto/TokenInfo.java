afc562c98158625d94f7fe26563a6a6d78cd0e03
Rafaello <baimurzinrafael02@gmail.com>
2022-03-04 23:26:08 +0600

    Refactored UserInfo

---
+++ src/main/java/com/socialNetwork/dto/TokenInfo.java
@@ -0,0 +1,15 @@
+package com.socialNetwork.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenInfo {
+    private String token;
+    private Date expirationTime;
+}
